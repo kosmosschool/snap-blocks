dd8b52fb687b3e1da83bd180d9ea69f559b45529
Can Olcer <shafyy@gmail.com>
2020-07-18 18:51:20 +0200

    added special modes to keyboard

--- scripts/keyboard.gd
+++ scripts/keyboard.gd
@@ -5,8 +5,8 @@ class_name Keyboard
 
 
 var keyboard_layout = [
-	["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-	["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+	["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace"],
+	["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "enter"],
 	["a", "s", "d", "f", "g", "h", "j", "k", "l"],
 	["shift", "z", "x", "c", "v", "b", "n", "m"],
 	["space"],
@@ -15,6 +15,7 @@ var keyboard_layout = [
 var start_offset = Vector3(-0.175, 0.05, 0.0)
 var x_offset = 0.04
 var y_offset = -0.04
+var shift_mode = false
 
 onready var buttons_node = $Buttons
 onready var key_button_scene = preload("res://scenes/ks_button_keyboard.tscn")
@@ -28,8 +29,21 @@ func _ready():
 func key_pressed(key_value : String):
 	# called by individual keys
 	# excepts keyboard_output_label to be a 2DTextLabel
+	
 	var prev_output = keyboard_output_label.get_text()
-	keyboard_output_label.set_text(prev_output + key_value)
+	
+	match key_value:
+		"space":
+			keyboard_output_label.set_text(prev_output + " ")
+		"backspace":
+			keyboard_output_label.set_text(prev_output.rstrip(1))
+		"shift":
+			shift_mode = true
+		_:
+			if shift_mode:
+				key_value = key_value.capitalize()
+			keyboard_output_label.set_text(prev_output + key_value)
+			shift_mode = false
 
 
 func init_keyboard():
